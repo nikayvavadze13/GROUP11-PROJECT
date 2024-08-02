88fa988deb284217fc1e0529368d534e28ca591c
GuramiPapunashvili <157598807+GuramiPapunashvili@users.noreply.github.com>
2024-08-03 03:41:34 +0400

    Iphone

---
+++ groupwork.js
@@ -0,0 +1,15 @@
+function Iphone15() {
+    window.location.href = "https://www.apple.com/iphone-15-pro/"
+}
+
+function Iphone14() {
+    window.location.href = "https://www.apple.com/iphone-14/specs/"
+}
+
+function Iphone13() {
+    window.location.href = "https://www.apple.com/iphone-13/specs/"
+}
+
+function Iphone12() {
+    window.location.href = "https://www.apple.com/iphone-se/"
+}
